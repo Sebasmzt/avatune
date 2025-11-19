530d58a587f217c886603107230a4132b662ea7b
Teimur Gasanov <teymurgg321@gmail.com>
2025-11-19 16:01:31 +0600

    feat: add react native assets support to flat design

---
+++ packages/flat-design-assets/src/react-native.ts
@@ -0,0 +1,86 @@
+// Body
+import BodyShirt from './svg/body/shirt.svg?native'
+import BodySweater from './svg/body/sweater.svg?native'
+import BodyTshirt from './svg/body/tshirt.svg?native'
+import BodyTurtleneck from './svg/body/turtleneck.svg?native'
+
+// Ears
+import EarsStandard from './svg/ears/standard.svg?native'
+
+// Eyebrows
+import EyebrowsAngry from './svg/eyebrows/angry.svg?native'
+import EyebrowsSmall from './svg/eyebrows/small.svg?native'
+import EyebrowsStandard from './svg/eyebrows/standard.svg?native'
+
+// Eyes
+import EyesBoring from './svg/eyes/boring.svg?native'
+import EyesDots from './svg/eyes/dots.svg?native'
+import EyesOpenCircle from './svg/eyes/openCircle.svg?native'
+import EyesOpenRounded from './svg/eyes/openRounded.svg?native'
+
+// Hair
+import HairBobRounded from './svg/hair/bobRounded.svg?native'
+import HairBobStraight from './svg/hair/bobStraight.svg?native'
+import HairCupCurly from './svg/hair/cupCurly.svg?native'
+import HairLong from './svg/hair/long.svg?native'
+import HairMedium from './svg/hair/medium.svg?native'
+import HairShort from './svg/hair/short.svg?native'
+
+// Head
+import HeadOval from './svg/head/oval.svg?native'
+
+// Mouth
+import MouthBigSmile from './svg/mouth/bigSmile.svg?native'
+import MouthFlat from './svg/mouth/flat.svg?native'
+import MouthFrown from './svg/mouth/frown.svg?native'
+import MouthHalfOpen from './svg/mouth/halfOpen.svg?native'
+import MouthLaugh from './svg/mouth/laugh.svg?native'
+import MouthNervous from './svg/mouth/nervous.svg?native'
+import MouthSmile from './svg/mouth/smile.svg?native'
+
+// Noses
+import NosesBig from './svg/noses/big.svg?native'
+import NosesCurve from './svg/noses/curve.svg?native'
+import NosesDots from './svg/noses/dots.svg?native'
+import NosesHalfOval from './svg/noses/halfOval.svg?native'
+
+export {
+  // Body
+  BodyShirt,
+  BodySweater,
+  BodyTshirt,
+  BodyTurtleneck,
+  // Ears
+  EarsStandard,
+  // Eyebrows
+  EyebrowsAngry,
+  EyebrowsSmall,
+  EyebrowsStandard,
+  // Eyes
+  EyesBoring,
+  EyesDots,
+  EyesOpenCircle,
+  EyesOpenRounded,
+  // Hair
+  HairBobRounded,
+  HairBobStraight,
+  HairCupCurly,
+  HairLong,
+  HairMedium,
+  HairShort,
+  // Head
+  HeadOval,
+  // Mouth
+  MouthBigSmile,
+  MouthFlat,
+  MouthFrown,
+  MouthHalfOpen,
+  MouthLaugh,
+  MouthNervous,
+  MouthSmile,
+  // Noses
+  NosesBig,
+  NosesCurve,
+  NosesDots,
+  NosesHalfOval,
+}
