41808f4423b3916a36402140388df44ae145c389
Teimur Gasanov <teymurgg321@gmail.com>
2025-11-28 12:59:55 +0600

    fix: release errors

--- packages/assets/miniavs-assets/rslib.shared.ts
+++ packages/assets/miniavs-assets/rslib.shared.ts
@@ -8,7 +8,6 @@ export const svgoConfig: SvgoConfig = {
       name: 'preset-default',
       params: {
         overrides: {
-          removeViewBox: false,
           cleanupIds: false,
         },
       },
